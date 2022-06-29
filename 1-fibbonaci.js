2cef57121353da7f253f6b08ebfc056ce47ca6be
usman <usmangavandi5445@gmail.com>
2022-06-29 22:39:52 +0530

    1

---
+++ 1-fibbonaci.js
@@ -0,0 +1,11 @@
+const input = 5;
+
+function findFibo(n) {
+  const start = [0, 1];
+  for (let i = 2; i < n; i++) {
+    start.push(start[i - 1] + start[i - 2]);
+  }
+  return start[start.length - 1];
+}
+
+console.log(findFibo(input));
