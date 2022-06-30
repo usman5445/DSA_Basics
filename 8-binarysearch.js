37e0e326167dc1ab57b2e0e6d12ded2fae2bb4b7
usman <usmangavandi5445@gmail.com>
2022-06-30 15:09:38 +0530

    8

---
+++ 8-binarysearch.js
@@ -0,0 +1,14 @@
+function binarySearch(arr, n) {
+  let start = 0;
+  let end = arr.length;
+  while (start < end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] == n) return mid;
+    else if (arr[mid] > n) end = mid;
+    else if (arr[mid] < n) start = mid;
+  }
+}
+
+console.log(binarySearch([2, 4, 5, 6, 8, 10], 8));
+console.log(binarySearch([2, 4, 5, 6, 8, 10], 10));
+console.log(binarySearch([2, 4, 5, 6, 8, 10], 2));
